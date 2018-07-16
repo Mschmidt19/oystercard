0f6f111b92c6522c82c7153bcc289646eb495c7a
Marcus <marcus.kerr94@gmail.com>
2018-07-16 15:59:15 +0100

    Feature: topup, wrote unit test to  rsponds to topup with 1 argument (RED)

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,10 @@ describe OysterCard do
   describe '#balance' do
     it { is_expected.to respond_to :balance }
   end
+
+  describe "#topup" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+  end 
+
+
 end
